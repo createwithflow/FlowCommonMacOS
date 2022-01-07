7f1a3151fa464223df318e15949ff76badab1b5a
Travis <travis@createwithflow.com>
2022-01-07 12:54:43 -0800

    Initial Commit

---
+++ FlowCommonMacOS/FlowCommonMacOS.h
@@ -0,0 +1,18 @@
+//
+//  FlowCommonMacOS.h
+//  FlowCommonMacOS
+//
+//  Created by Travis on 2022-01-07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlowCommonMacOS.
+FOUNDATION_EXPORT double FlowCommonMacOSVersionNumber;
+
+//! Project version string for FlowCommonMacOS.
+FOUNDATION_EXPORT const unsigned char FlowCommonMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowCommonMacOS/PublicHeader.h>
+
+
